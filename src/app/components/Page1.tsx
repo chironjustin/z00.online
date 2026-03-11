86aa180371315e2e12139e1ac44cef0a9f91f8bf
chironjustin <chironjustin@gmail.com>
2026-03-11 19:49:02 +0100

    Update Page1.tsx

--- src/app/components/Page1.tsx
+++ src/app/components/Page1.tsx
@@ -1,8 +1,10 @@
+const pageImage = '/assets/page-image.png';
+
 export default function Page1() {
   return (
     <div className="fixed inset-0 bg-black flex items-center justify-center">
       <img 
-        src="/assets/page1-image.jpg"
+        src={pageImage}
         alt="Page 1"
         className="max-w-[90vw] max-h-[90vh] object-contain"
       />
