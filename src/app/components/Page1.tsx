e2339cd8605758a668efb399a3f66851f24870b4
chironjustin <chironjustin@gmail.com>
2026-03-11 18:10:56 +0100

    Update Page1.tsx

--- src/app/components/Page1.tsx
+++ src/app/components/Page1.tsx
@@ -1,10 +1,8 @@
-import pageImage from 'figma:asset/78c72e1cfb526784c406f68ce67984b5a0a58c41.png';
-
 export default function Page1() {
   return (
     <div className="fixed inset-0 bg-black flex items-center justify-center">
       <img 
-        src={pageImage}
+        src="/assets/page1-image.jpg"
         alt="Page 1"
         className="max-w-[90vw] max-h-[90vh] object-contain"
       />
