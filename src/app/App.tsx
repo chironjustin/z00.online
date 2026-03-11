11c6997e47792f1833e846389f7cbdd830136185
chironjustin <chironjustin@gmail.com>
2026-03-11 20:16:38 +0100

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,5 @@
-import { RouterProvider } from 'react-router';
-import { router } from './routes';
+import Home from './components/Home';
 
 export default function App() {
-  return <RouterProvider router={router} />;
+  return <Home />;
 }
